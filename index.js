2464ec5c627ece3249b8e3a1e53530a8f8fc4f49
Fernando Serrano <fernandojsg@gmail.com>
2017-04-07 13:00:50 +0200

    Added vive

--- index.js
+++ index.js
@@ -6,12 +6,49 @@ if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
 
+AFRAME.registerComponent('triline', {
+  schema: {
+    start: {type: 'vec3', default: '0 0 0'},
+    end: {type: 'vec3', default: '0 0 0'},
+    color: {type: 'color', default: '#fff'}
+  },
+
+  init: function () {
+    var material = this.material = new THREE.LineBasicMaterial({color: this.data.color});
+    var geometry = this.geometry = new THREE.BufferGeometry();
+    geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2 * 3), 3));
+
+    this.line = new THREE.Line(geometry, material);
+    this.el.setObject3D('line', this.line);
+  },
+
+  update: function (oldData) {
+    var data = this.data;
+    var diff = AFRAME.utils.diff(data, oldData);
+
+    // Update geometry
+    if ('start' in diff || 'end' in diff) {
+      var pos = this.geometry.attributes.position.array;
+      pos[0] = data.start.x;
+      pos[1] = data.start.y;
+      pos[2] = data.start.z;
+      pos[3] = data.end.x;
+      pos[4] = data.end.y;
+      pos[5] = data.end.z;
+      this.geometry.attributes.position.needsUpdate = true;
+    }
+
+    this.material.color.setStyle(data.color);
+  }
+});
+
 /**
  * Tooltip component for A-Frame.
  */
 AFRAME.registerComponent('tooltip', {
   schema: {
     text: {default: ''},
+    end: {type: 'vec3'},
     width: {default: 1, min: 0},
     height: {default: 1, min: 0},
     lineHorizontalAlign: {default: 'left', oneOf: ['left', 'right', 'center']},
@@ -29,13 +66,27 @@ AFRAME.registerComponent('tooltip', {
   init: function () {
     var el = this.el;
     var data = this.data;
+
+    // Line
+    var material = this.material = new THREE.LineBasicMaterial({color: this.data.color});
+    var geometry = this.geometry = new THREE.BufferGeometry();
+    geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2 * 3), 3));
+
+    this.line = new THREE.Line(geometry, material);
+    this.el.setObject3D('line', this.line);
+
+
+
+
+
+
     var line = this.line = document.createElement('a-entity');
-    line.setAttribute('line', {start: '0 0 0', end: '-2 -1 -3'});
+    line.setAttribute('line', {end: data.targetPosition});
     el.appendChild(line);
 
     var quad = this.quad = document.createElement('a-entity');
-    quad.setAttribute('slice9', {width: data.width, height: data.height, left: 20, right: 43, top: 20, bottom: 43, padding: 0.02, src: 'tooltip.png'});
-    quad.setAttribute('text', {width: 0.9, color: '#fff', value: data.text, align: 'center'});
+    quad.setAttribute('slice9', {width: data.width, height: data.height, left: 20, right: 43, top: 20, bottom: 43, padding: 0.005, src: 'tooltip.png'});
+    quad.setAttribute('text', {width: 0.25, color: '#fff', value: data.text, align: 'center'});
     el.appendChild(quad);
   },
 
@@ -51,7 +102,7 @@ AFRAME.registerComponent('tooltip', {
     var y = valign[data.lineVerticalAlign];
     var x = halign[data.lineHorizontalAlign];
 
-    var endPosition = data.endPosition;
+    var endPosition = data.targetPosition;
 
     this.line.setAttribute('line', {start: {x: x, y: y, z: 0}, end: endPosition});
   }
